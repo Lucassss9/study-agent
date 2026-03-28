6c98eb2479eac1ab0d4a4311388c740c46287053
lucassss9 <lucasgabriel09104@gmail.com>
2026-03-27 21:16:38 -0300

    feat: add ContentController with CRUD endpoints

---
+++ src/main/java/com/studyagent/controller/ContentController.java
@@ -0,0 +1,49 @@
+package com.studyagent.controller;
+
+import com.studyagent.model.Content;
+import com.studyagent.service.ContentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/study-blocks/{blockId}/contents")
+public class ContentController {
+
+    private final ContentService contentService;
+
+    public ContentController(ContentService contentService) {
+        this.contentService = contentService;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Content buscarPorId(@PathVariable Long id) {
+        return contentService.buscarPorId(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Content salvarConteudo(@PathVariable Long blockId, @RequestBody Content content) {
+        return contentService.salvar(content, blockId);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Content atualizarConteudo(@PathVariable Long id, @RequestBody Content dataNew) {
+        return contentService.atualizar(id, dataNew);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarConteudo(@PathVariable Long id) {
+        contentService.delete(id);
+    }
+}
