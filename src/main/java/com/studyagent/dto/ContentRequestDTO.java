fb3174eb744b2aa12cfb7463dc6646dbcaabbf70
lucassss9 <lucasgabriel09104@gmail.com>
2026-03-29 09:17:12 -0300

    fix: set dateTime automatically using LocalDateTime.now()

--- src/main/java/com/studyagent/dto/ContentRequestDTO.java
+++ src/main/java/com/studyagent/dto/ContentRequestDTO.java
@@ -4,13 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ContentRequestDTO {
 
     private String title;
-    private LocalDateTime dateTime;
 }
